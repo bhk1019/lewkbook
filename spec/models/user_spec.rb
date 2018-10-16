2a0f4514ca54fc6cf3bf67d385d58bc582285cc2
bhk1019 <bhk1019@gmail.com>
2018-10-15 19:29:49 -0700

    set up user password validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe User, type: :model do
 	
 	describe "new user" do
 
-		context "with valid name and email" do
+		context "with all valid attributes" do
 			before(:each) do
-				@user = User.new(name: 'John Smith', email: 'example@example.com')
+				@user = User.new(name: 'John Smith', email: 'example@example.com', password: 'password', password_confirmation: 'password')
 			end
 			it "should be valid" do
 				expect(@user).to be_valid
@@ -17,11 +17,14 @@ RSpec.describe User, type: :model do
 			it "should have short enough name" do
 				expect(@user.name.length).to be < 50
 			end
+			it "should have long enough password" do
+				expect(@user.password.length).to be > 6
+			end
 		end
 
 		context "with valid email" do
 			before(:each) do
-				@user = User.new(name: 'John Smith', email: 'exaMple@example.com')
+				@user = User.new(name: 'John Smith', email: 'exaMple@example.com', password: 'password', password_confirmation: 'password')
 			end
 			it "should have short enough email" do
 				expect(@user.email.length).to be < 255
@@ -77,10 +80,22 @@ RSpec.describe User, type: :model do
 			end
 		end
 
+		context "with no password" do
+			before(:each) do
+				@user = User.new(name: "john smith", email: "user@example.com")
+			end
+			it "should be invalid" do
+				expect(@user).to be_invalid
+			end
+		end
 
-
-
+		context "with short password" do
+			before(:each) do
+				@user = User.new(name: "john smith", email: "user@example.com", password: '123', password_confirmation: '123')
+			end
+			it "should be invalid" do
+				expect(@user).to be_invalid
+			end
+		end
 	end
-
-
 end
